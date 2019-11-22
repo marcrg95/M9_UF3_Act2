a80c3c84a9e28c20bf16c6823a370becb3b12702
formu <formu@ASUS-LAPTOP>
2019-11-22 18:39:16 +0100

    1.0.1 S'afegeix la classe Exemple2URL

---
+++ src/uf3/Exemple2URL.java
@@ -0,0 +1,32 @@
+package uf3;
+
+import java.net.*;
+import java.io.*;
+
+public class Exemple2URL {
+	public static void main (String[] args) {
+		URL url=null;
+		
+		try {
+			
+			url = new URL("http://www.insbaixcamp.cat/moodle/");
+			
+		} catch (MalformedURLException e) {e.printStackTrace(); }
+		
+		BufferedReader in;
+		
+		try {
+			
+			InputStream inputStream = url.openStream();
+			in = new BufferedReader(new InputStreamReader(inputStream));
+			
+			String inputLine;
+			
+			while ((inputLine = in.readLine()) != null)
+				System.out.println(inputLine);
+			in.close();
+			
+		} catch (IOException e) {e.printStackTrace(); }
+
+	}
+}
